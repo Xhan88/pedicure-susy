84e6ebefa0a02bbcd488378f1e53e606becbdaea
Christian Martinez <dantedeat@hotmail.com>
2023-03-31 18:35:14 -0600

    nop

--- src/img-presentation/NavImg.jsx
+++ src/img-presentation/NavImg.jsx
@@ -2,9 +2,18 @@ import '../img-presentation/NavImg.css'
 
 const NavImg = () => {
   return (
-    <>
+    <div className='container'>
       <img className='img-header' src="/header2.jpg" alt="" />
-    </>
+      <div className='presentation-text'></div>
+      <div className='center'>
+        <h1>Pedicure Clinico</h1>
+        <p>Atendidos por su propietaria y experta
+           en cuidado para los pies Susy Sanchez</p>
+           <button className='btn-contact'>
+            CONTACTAME
+           </button>
+        </div>
+    </div>
   )
 }
 
