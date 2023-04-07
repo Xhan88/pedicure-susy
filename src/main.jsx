06800664adc33b1b8ff8e28c84ba464046ef1039
Christian Martinez <dantedeat@hotmail.com>
2023-04-07 08:12:12 -0600

    chan

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import AutoPlaySwipeableViews from './Gallery/AutoPlaySwipeableViews'
 import NavImg from './img-presentation/NavImg'
 import './index.css'
 import NavBarFirst from './NavBar-1/NavBarFirst'
 import NavBarSecond from './NavBar-2/NavBarSecond'
 import SecctionOne from './section-1/SecctionOne'
 import SectionTwo from './section-2/SectionTwo'
+import SectionThree from './section-3/SectionThree'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -15,5 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <NavImg />
     <SecctionOne />
     <SectionTwo />
+    <SectionThree />
+    <AutoPlaySwipeableViews />
   </React.StrictMode>,
 )
---
+++ src/section-3/SectionThree.jsx
@@ -0,0 +1,15 @@
+import '../section-3/SectionThree.css';
+
+const SectionThree = () => {
+  return (
+    <div className='title-section-three' style={{marginTop: 180}}>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, voluptatibus!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aliquid minus dolorum nostrum quaerat excepturi maiores? Temporibus id aliquid ratione eligendi ut eius? Necessitatibus laudantium officiis quam quis accusantium iste!</p>
+      <div>
+        <img style={{marginTop: -450, marginLeft: 400, paddingBottom: 100}} src="/feet-nice.png" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default SectionThree
