e4b42184d3bb1bd938b23cfd8580e342066fc5a4
Christian Martinez <dantedeat@hotmail.com>
2024-09-07 06:39:51 -0600

    7 ags 24

--- src/section-3/SectionThree.jsx
+++ src/section-3/SectionThree.jsx
@@ -2,12 +2,12 @@ import '../section-3/SectionThree.css';
 
 const SectionThree = () => {
   return (
-    <div className='title-section-three' style={{marginTop: 220}}>
+    <div className='title-section-three'                                                                                                                                                                            >
       <h1>¿Qué incluye el servicio de pedicure Clinico?</h1>
       <p>
       Un auténtico tratamiento de lujo de entre 1 y 2 horas para pies que incluye exfoliación e hidratación, cutículas,corte de uñas, masaje, limado    y  esmaltado. Todo ello, acompañado de música relajante, un espacio cómodo y una tasa de cafe</p>
       <div>
-        <img style={{marginTop: -450, marginLeft: 400, paddingBottom: 100}} src="/feet-nice.png" alt="" />
+        <img className='img-feet-nice' src="/feet-nice.png" alt="" />
         <hr style={{marginBottom: 50}}/>
       </div>
     </div>
